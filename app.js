3fc91e31735cf70c2f620f68338bf338ebecb3b5
Saravanan Naganathan <saravanan.n009@gmail.com>
2019-11-06 12:54:59 -0600

    updating the events

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ function cameraStart() {
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
+    cameraStart();
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
