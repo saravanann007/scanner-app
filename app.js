b093c742d539cc768b36652f3e11ad0f0da7d2a9
Saravanan Naganathan <saravanan.n009@gmail.com>
2019-11-06 18:23:03 -0600

    Update app.js
    app

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ function UserAction() {
 		spinner.style.visibility="hidden";
          if (this.readyState == 4 && this.status == 200) {
              console.log(this.responseText);
+		 checkmark.style.visibility='visible';
          document.getElementById("checkmark").innerHTML='<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>'+
             '<path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>';
 
