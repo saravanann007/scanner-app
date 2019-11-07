31d060136fb795852d3c30ca1b63510e010c0d3a
Vivekrn <49915113+Vivekrn@users.noreply.github.com>
2019-11-07 16:53:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -49,6 +49,13 @@ function UserAction() {
             '<path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>'+
 		 '<p class="success">Cheers!!</p>';
 	     }
+	    else if (this.status !== 200)
+	    {
+             document.getElementById("checkmark").innerHTML='<svg class="checkmark_cross" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">'+
+            '<circle class="checkmark__redcircle" cx="26" cy="26" r="25" fill="none" />'+
+            '<path class="checkmark__check" fill="none" d="M16 16 36 36 M36 16 16 36" /></svg>'+
+			'<p class="failure">Error!</p>';
+	    }
     };
     xhttp.open("GET", "https://172.20.10.6:8443/pour", true);
     xhttp.setRequestHeader("Content-type", "application/json");
