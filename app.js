37a531cf579f812660e4d315b73ef4259b880eb5
Saravanan Naganathan <saravanan.n009@gmail.com>
2019-11-06 22:53:23 -0600

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function UserAction() {
             document.getElementById("checkmark").innerHTML='<svg class="checkmark_cross" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">'+
             '<circle class="checkmark__redcircle" cx="26" cy="26" r="25" fill="none" />'+
             '<path class="checkmark__check" fill="none" d="M16 16 36 36 M36 16 16 36" /></svg>'+
-		    '<p class="success">Sorry!</p>';
+		    '<p class="failure">Sorry!</p>';
          }
          }
 
